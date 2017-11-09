63d4c0d5d6dd56c7c9cbb24b57d9719095ce434d
zhuyonghao <zhuyonghao@vmcshop.com>
2017-11-09 13:29:10 +0800

    初始化项目

---
+++ app/src/main/java/com/kotlin/sample/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.kotlin.sample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import org.jetbrains.anko.button
+import org.jetbrains.anko.editText
+import org.jetbrains.anko.sdk25.coroutines.onClick
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.verticalLayout
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        verticalLayout {
+            val name = editText()
+            button("Say Hello") {
+                onClick { toast("Hello, ${name.text}!") }
+            }
+        }
+    }
+}
